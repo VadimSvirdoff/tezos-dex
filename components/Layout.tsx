d03556d7ae67811484dbef04595d1c9331c956d0
Vadym Svyrydov <avangardsv@gmail.com>
2021-07-03 15:43:50 +0300

    add page template

---
+++ components/Layout.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import styles from '../styles/layout.module.css'
+import React from 'react'
+import { ChakraProvider, Box, VStack, Grid, theme } from "@chakra-ui/react";
+
+
+const name = 'Vadym Svyrydov'
+export const siteTitle = 'Next.js Sample Website'
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div >
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Box fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          {children}
+
+        </Grid>
+      </Box>
+    </div>
+  )
+}
